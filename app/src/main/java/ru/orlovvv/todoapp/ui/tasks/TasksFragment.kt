957d3c421e1e00466d0c13eaecca67fd88710d11
unknown <qu4dro.sl@gmail.com>
2021-03-09 14:30:59 +0800

    Fragmen updated, now it can show list from db

--- app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksFragment.kt
@@ -1,12 +1,36 @@
 package ru.orlovvv.todoapp.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ru.orlovvv.todoapp.R
+import ru.orlovvv.todoapp.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            rvTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.task.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
