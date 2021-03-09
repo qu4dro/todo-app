17df837dacdec466392fc8867bfb7dd07d60e1ea
unknown <qu4dro.sl@gmail.com>
2021-03-09 11:27:51 +0800

    Task DAO created

---
+++ app/src/main/java/ru/orlovvv/todoapp/data/db/TaskDao.kt
@@ -0,0 +1,21 @@
+package ru.orlovvv.todoapp.data.db
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import ru.orlovvv.todoapp.data.db.entities.TaskItem
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(task: TaskItem)
+
+    @Update
+    suspend fun update(task: TaskItem)
+
+    @Delete
+    suspend fun delete(task: TaskItem)
+
+    @Query("SELECT * FROM tasks_table")
+    fun getAllTasks(): Flow<List<TaskItem>>
+}
