bd707ea9e2cfc988f18cfffcf4ff44ecbe8f04e1
unknown <qu4dro.sl@gmail.com>
2021-03-09 14:30:11 +0800

    Viewmodel upgraded with asLiveData

--- app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksViewModel.kt
@@ -1,6 +1,7 @@
 package ru.orlovvv.todoapp.ui.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.orlovvv.todoapp.data.db.TaskDao
 import javax.inject.Inject
@@ -8,5 +9,6 @@ import javax.inject.Inject
 @HiltViewModel
 class TasksViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
 
+    val task = taskDao.getAllTasks().asLiveData()
 
 }
