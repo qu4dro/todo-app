75673a09cb800e90f9647b604647e1b9b3d9ea86
unknown <qu4dro.sl@gmail.com>
2021-03-10 13:04:10 +0800

    Updated with flow string query

--- app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksViewModel.kt
@@ -3,12 +3,18 @@ package ru.orlovvv.todoapp.ui.tasks
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import ru.orlovvv.todoapp.data.db.TaskDao
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
 
-    val task = taskDao.getAllTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getAllTasks(it)
+    }
+    val task = tasksFlow.asLiveData()
 
 }
