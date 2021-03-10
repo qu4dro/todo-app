2697c6c97b08ff2ed6cb76ad3634585aa361c8b0
unknown <qu4dro.sl@gmail.com>
2021-03-10 12:53:19 +0800

    Class with extensions created

---
+++ app/src/main/java/ru/orlovvv/todoapp/util/ViewExt.kt
@@ -0,0 +1,16 @@
+package ru.orlovvv.todoapp.util
+
+import androidx.appcompat.widget.SearchView
+
+inline fun SearchView.onTextChanged(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            listener(newText.orEmpty())
+            return true
+        }
+    })
+}
