e4bdd66051c97107d9f2b9eeed2a8fe1412422c4
unknown <qu4dro.sl@gmail.com>
2021-03-10 14:38:59 +0800

    Class updated with flow

--- app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import ru.orlovvv.todoapp.data.db.TaskDao
 import javax.inject.Inject
@@ -12,9 +13,22 @@ import javax.inject.Inject
 class TasksViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getAllTasks(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getAllTasks(query, sortOrder, hideCompleted)
     }
     val task = tasksFlow.asLiveData()
 
+}
+
+enum class SortOrder() {
+    BY_NAME,
+    BY_DATE
 }
