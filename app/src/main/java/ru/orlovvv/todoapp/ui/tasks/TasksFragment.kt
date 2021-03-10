9fa8624092dc1f5107cb439516a5db28c8a29879
unknown <qu4dro.sl@gmail.com>
2021-03-10 13:03:33 +0800

    Added menu functions

--- app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/ru/orlovvv/todoapp/ui/tasks/TasksFragment.kt
@@ -1,13 +1,18 @@
 package ru.orlovvv.todoapp.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ru.orlovvv.todoapp.R
 import ru.orlovvv.todoapp.databinding.FragmentTasksBinding
+import ru.orlovvv.todoapp.util.onTextChanged
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
@@ -31,6 +36,37 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         viewModel.task.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_sort_by_date -> {
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            else ->  super.onOptionsItemSelected(item)
         }
     }
 }
